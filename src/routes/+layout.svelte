06df682a49424debca19939e09994bc8a099c3b9
BozzleMay <ledley26@msn.com>
2024-05-13 19:36:58 +0100

    arrow href

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 <div class="relative flex-column max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
     <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + ( y > 0 ? "opacity-full pointer-events-auto" : "opacity-0 pointer-events-none")}>
         <button on:click={goTop} class="ml-auot rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer">
-            <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
+            <a href="#top"><i class="fa-solid fa-arrow-up grid place-items-center aspect-square" /></a>
         </button>
     </div>
     <Header y={y}/>
